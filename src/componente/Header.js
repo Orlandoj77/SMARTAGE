3226b3311d11d585293ace794cfd1d3ad0209998
p.capitango65@gmail.com <orlandolaudes7@gmail.com>
2021-06-30 03:49:14 +0100

    prieiro commit

---
+++ src/componente/Header.js
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <div>
+      <div>
+        {" "}
+        <h3>Find a Store</h3>
+      </div>
+
+      <h1>SMARTAGE</h1>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import Header from "../componente/Header";
+
+function Home() {
+  return (
+    <div>
+      <Header />
+      <p>Hello world</p>
+    </div>
+  );
+}
+export default Home;
